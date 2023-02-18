dd6e2b49434d3a5ffe6fd104d0e40f9c64f863fd
sozysozbot <sozysozbot@gmail.com>
2023-02-18 22:52:06 +0900

    Rename

--- main.c
+++ main.c
@@ -28,10 +28,10 @@ void butterfly(double complex *pa, double complex *pb, double complex w) {
 
 double complex W(int m, int N) { return cexp(-2 * atan(1) * 4 * I * m / N); }
 
-void fft(int N, const double complex in_[N], double complex out[N]) {
-  double complex in[N];
+void fft(int N, const double complex in[N], double complex out[N]) {
+  double complex tmp[N];
   for (int i = 0; i < N; i++) {
-    in[i] = in_[i];
+    tmp[i] = in[i];
   }
   int log2_N = log2(N);
   if (exp2(log2_N) != N) {
@@ -43,12 +43,12 @@ void fft(int N, const double complex in_[N], double complex out[N]) {
     int mask = N >> (stage + 1);
     for (int i = 0; i < N; i++) {
       if (i & mask) continue;
-      butterfly(&in[i], &in[i | mask], W(i & (mask - 1), N >> stage));
+      butterfly(&tmp[i], &tmp[i | mask], W(i & (mask - 1), N >> stage));
     }
   }
 
   for (int i = 0; i < N; i++) {
-    out[i] = in[reverse_bits(log2(N), i)];
+    out[i] = tmp[reverse_bits(log2(N), i)];
   }
 }
 
